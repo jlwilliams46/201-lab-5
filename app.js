26b04c8a212491c4fcf07fb32f8ec14faaa042fd
jlwilliams46 <jwilliams492@gmail.com>
2017-10-23 03:41:42 -0700

    first submission. still require additional help

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+// TODO: Done
 function sum(a,b){ //eslint-disable-line
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
@@ -18,7 +18,7 @@ function sum(a,b){ //eslint-disable-line
 }
 
 // TODO: Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,13 +30,29 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
-function multiply(a,b){ //eslint-disable-line
+// TODO: DONE
+var product = 0;
+var num1 = 5;
+var num2 = 9;
+var arr = [];
+var str;
 
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
+product = multiply(num1, num2);
+
+arr.push(product);
+
+str = 'The product of ' + num1 + ' and ' + num2 + ' is ' + product + '.';
+
+arr.push(str);
+console.log(arr);
+
+
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
